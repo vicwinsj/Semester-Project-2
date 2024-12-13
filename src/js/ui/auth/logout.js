d4aa3defd19760a6bfbf94f31522bdaf8e950ae8
Victor Winsjansen <victor.winsjansen@icloud.com>
2024-12-13 15:43:16 +0100

    Another link fix

--- src/js/ui/auth/logout.js
+++ src/js/ui/auth/logout.js
@@ -1,8 +1,15 @@
+import { GH_BASE } from "../../api/constants";
+
 export function onLogout(event) {
   event.preventDefault();
   const isLoggedIn = localStorage.accessToken;
   if (isLoggedIn) {
     localStorage.removeItem("accessToken");
-    location.reload();
+
+    if (window.location.pathname === `${GH_BASE}/profile/`) {
+      window.location.href = `${GH_BASE}`;
+    } else {
+      location.reload();
+    }
   }
 }
