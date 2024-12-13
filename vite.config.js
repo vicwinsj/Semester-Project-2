4e97efd033ea105c23dd4edcc3ddd0dd72980e96
Victor Winsjansen <victor.winsjansen@icloud.com>
2024-12-13 18:46:14 +0100

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,35 @@
 import { defineConfig } from "vite";
 
-const isGitHubPages = location.hostname.includes("github.io");
+export default defineConfig(({ mode }) => {
+  const isGitHubPages = mode === "production";
+  const base = isGitHubPages ? "/Semester-Project-2/" : "/";
 
-export default defineConfig({
-  appType: "mpa",
-  base: isGitHubPages ? "/Semester-Project-2/" : "/",
-  css: {
-    devSourcemap: true,
-  },
-  build: {
-    target: "esnext",
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        login: "./auth/login/index.html",
-        register: "./auth/register/index.html",
-        profile: "./profile/index.html",
-        listing: "./listing/index.html",
-        createListing: "./listing/create/index.html",
+  return {
+    appType: "mpa",
+    base,
+    css: {
+      devSourcemap: true,
+    },
+    build: {
+      target: "esnext",
+      outDir: "dist",
+      rollupOptions: {
+        input: {
+          main: "./index.html",
+          login: "./auth/login/index.html",
+          register: "./auth/register/index.html",
+          profile: "./profile/index.html",
+          listing: "./listing/index.html",
+          createListing: "./listing/create/index.html",
+        },
       },
     },
-  },
-  server: {
-    host: true,
-    port: 5176,
-  },
-  resolve: {
-    alias: {},
-  },
+    server: {
+      host: true,
+      port: 5176,
+    },
+    resolve: {
+      alias: {},
+    },
+  };
 });
